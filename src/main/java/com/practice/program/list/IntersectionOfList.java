96dd7150605fe85b9f61771f003687c8f2d93011
rajansinghal <mr.rajansinghal@gmail.com>
2020-12-09 23:41:46 +0530

    intersection of lists

---
+++ src/main/java/com/practice/program/list/IntersectionOfList.java
@@ -0,0 +1,9 @@
+package com.practice.program.list;
+
+public class IntersectionOfList {
+    // approach 3
+    //https://medium.com/swlh/intersection-of-two-linked-lists-a920fe2ec7c2
+
+    // or also can try first iteration the bigger one untill remaining elements in both the list is same
+    // and from there iterate both of them 
+}
